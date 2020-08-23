5d4e7c130cab386755b438b5d822873fe461e966
dgowravaram <51542928+dgowravaram@users.noreply.github.com>
2020-08-23 15:59:58 -0400

    Add files via upload

---
+++ client/client.js
@@ -0,0 +1,21 @@
+console.log("hello world");
+
+const form = document.querySelector('form');
+const loadingElement = document.querySelector('.loading');
+
+loadingElement.style.display = 'none'
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const name = formData.get('name');
+  const content = formData.get('content');
+
+  const oink = {
+    name,
+    content
+  };
+  console.log(oink);
+  form.style.display = 'none';
+  loadingElement.style.display = ''
+})
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+// 30:15
+app.get('/', (req, res) => {
+
+});
+
+app.listen(5000, () => {
+  console.log('listening');
+});
